2e355f177cabf534109619c87af6a77c660d8052
Rick <rick.c.kremer@gmail.com>
2021-01-03 08:04:57 -0600

    Update keymap.c

--- gaming/keymap.c
+++ gaming/keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_GAME] = LAYOUT( \
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,   \
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,    \
-    KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                    KC_H,       GUI_J,      KC_K,       KC_L,       KC_SCLN,    KC_QUOT,   \
+    KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                    KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,   \
     KC_LCTRL,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_MUTE,             KC_MPLY,   KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,    \
-                      xxxxxxx,     _FN,      KC_LALT,   KC_SPC,      KC_ENT,                      RSE_SPC,        KC_SPC,       KC_RALT,    KC_RCTRL,    xxxxxxx                \
+                      xxxxxxx,     _FN,      KC_LALT,   KC_SPC,      KC_ENT,                      KC_SPC,         KC_SPC,       KC_RALT,    KC_RCTRL,    xxxxxxx                \
 ), 
 
 /* FN
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_FN] = LAYOUT( \
-    _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                    KC_F6       KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,   \
+    _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,   \
     _______,    _______,    MAKE,       _______,    RESET,      _______,                                  _______,    _______,    _______,    _______,    _______,    _______,   \
     _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,                                  _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,   \
     _______,    _______,    _______,    _______,    _______,    _______,    _______,          _______,    _______,    _______,    _______,    _______,    _______,    _______,   \
